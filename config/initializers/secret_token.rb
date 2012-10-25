5dac40cdae4b38e72b7973acf6228d2ea1635f43
Stephen Freiberg <safreiberg@gmail.com>
2012-10-25 13:27:39 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SafProj3::Application.config.secret_token = 'bd7380fb8fb1432128812d17cb1ea0bd011d41f17de6efe11484badf0ee163bd0e64f1711d56cde646786a084f35f822ceac84d768af9fa5429b9fc8e98c2ef4'
